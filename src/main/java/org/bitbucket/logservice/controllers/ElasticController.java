d82f93f14491169ccd86d50eef4e1cace9d87473
AlexDodul <alexdodul87@gmail.com>
2021-10-05 20:40:20 +0300

    test for heroku

--- src/main/java/org/bitbucket/logservice/controllers/ElasticController.java
+++ src/main/java/org/bitbucket/logservice/controllers/ElasticController.java
@@ -42,7 +42,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -184,7 +183,7 @@ public class ElasticController {
   }
 
   @GetMapping(path = "/token-from-bot")
-  public ResponseEntity<Object> getTokenBot(@RequestHeader OAuthV2AccessRequest code) {
+  public ResponseEntity<Object> getTokenBot(@RequestParam OAuthV2AccessRequest code) {
     System.out.println("Hello, logs!");
     log.error(code.getCode());
     App app = new App();
