fffd6b28a88c590797ba54371ef2c1c605694bfd
AlexDodul <alexdodul87@gmail.com>
2021-10-06 11:59:30 +0300

    test for heroku

--- src/main/java/org/bitbucket/logservice/controllers/SlackAppController.java
+++ src/main/java/org/bitbucket/logservice/controllers/SlackAppController.java
@@ -4,13 +4,10 @@ package org.bitbucket.logservice.controllers;
 import com.slack.api.bolt.App;
 import com.slack.api.bolt.servlet.SlackAppServlet;
 import javax.servlet.annotation.WebServlet;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
-@WebServlet("/slack/oauth")
+@WebServlet("/slack/events")
 public class SlackAppController extends SlackAppServlet {
   public SlackAppController(App app) {
-    super(app.asOAuthApp(true));
-    log.error("---=== /slack/oauth ===---");
+    super(app);
   }
 }
