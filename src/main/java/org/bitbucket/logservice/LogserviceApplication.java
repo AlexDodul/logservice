39d4fc5ccd16d9dee1d8e73e67d3fa8a2beddc50
AlexDodul <alexdodul87@gmail.com>
2021-10-06 10:00:08 +0300

    test for heroku

--- src/main/java/org/bitbucket/logservice/LogserviceApplication.java
+++ src/main/java/org/bitbucket/logservice/LogserviceApplication.java
@@ -1,17 +1,21 @@
 package org.bitbucket.logservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
+@Slf4j
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableElasticsearchRepositories(basePackages = "org.bitbucket.logservice.repositories")
 @ServletComponentScan
 public class LogserviceApplication {
 
   public static void main(String[] args) {
+    System.out.println("Hello, logs!");
+    log.error("-=My custom message");
     SpringApplication.run(LogserviceApplication.class, args);
   }
 
