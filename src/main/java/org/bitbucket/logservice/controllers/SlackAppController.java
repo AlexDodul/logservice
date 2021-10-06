fd82644e3a66d2c4afb89c2eee4aeae57d9dcc2f
AlexDodul <alexdodul87@gmail.com>
2021-10-06 13:49:12 +0300

    test for heroku

--- src/main/java/org/bitbucket/logservice/controllers/SlackAppController.java
+++ src/main/java/org/bitbucket/logservice/controllers/SlackAppController.java
@@ -5,7 +5,7 @@ import com.slack.api.bolt.App;
 import com.slack.api.bolt.servlet.SlackAppServlet;
 import javax.servlet.annotation.WebServlet;
 
-@WebServlet("/slack/install")
+@WebServlet("/slack/events")
 public class SlackAppController extends SlackAppServlet {
   public SlackAppController(App app) {
     super(app);
